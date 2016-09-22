58120059ed5dab9f0ddd5d31b85e7114c8124efd
gmsoft <admin@example.com>
2016-09-22 17:34:34 +0800

    重构支持watch model, 修复bug

--- ng-switch.js
+++ ng-switch.js
@@ -1,22 +1,45 @@
+
+//当有多个开关指令绑定同一个model时, 用于保存点击事件触发的指令scope
+//在watch中排除掉, 不触发UI的change (不然会引发重复触发的错误逻辑)
+var currentTargetScope;
+
 angular.module('gm.switch', [])
     .directive('switch', function(){
         return {
             restrict: 'EA',
-            template: "<div class=\"switch\"/><input type=\"checkbox\" ng-model=\"switch\" data-toggle=\"switch\" /></div>",
+            template: "<div class=\"switch\"/><input type=\"checkbox\" data-toggle=\"switch\"/></div>",
             scope: {
                 'switch': "="
             },
             link: function(scope, element){
 
+                //初始化jquery插件
                 element.bootstrapSwitch();
+
+                //设置初始状态
                 element.bootstrapSwitch('setState', scope['switch']);
 
-                element.on('click', function () {
-                    scope.$apply(function(){
-                        scope['switch'] = element.bootstrapSwitch('status');
-                    });
+                //绑定点击事件, 修改model值
+                element.find('label').on('click', function(){
 
-                })
+                    //保存当前触发点击事件的指令scope
+                    currentTargetScope = scope;
+
+                    scope.$apply(function(){
+                        scope['switch'] = !scope['switch'];
+                    });
+                });
+
+                //监测model的变化,刷新UI
+                scope.$watch('switch', function(newValue, oldValue){
+
+                    //初始化和自己触发的model改变不刷新UI
+                    if(newValue !== oldValue && currentTargetScope !== scope)
+                        element.bootstrapSwitch('setState', newValue);
+
+                    //清空currentTargetScope, 避免外部修改model值时不能刷新UI
+                    currentTargetScope = null;
+                });
 
             }
         };
